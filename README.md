7c7e5cd4440fe3a8540eb9af1bbdd5b139de3414
guptaro <rohit.gupta@powerschool.com>
2019-10-20 06:57:34 +0530

    base 2

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # exetron
+
 To detect the exercises from a given video stream
+
+1. copy yolo model `yolo.h5` to `generate_boxes\model_data`
+2. copy video data to `.\input`
+3. cd generate_boxes
+4. python demo.py
+5. observer output in `.\output.txt`
--- generate_boxes/demo.py
+++ generate_boxes/demo.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import division, print_function, absolute_import
+import pandas as pd
 
 import os
+import glob
 from timeit import time
 import warnings
 import sys
@@ -22,9 +24,20 @@ from deep_sort.detection import Detection as ddet
 warnings.filterwarnings('ignore')
 
 
-def main(yolo):
+def doOverlap(l1, r1, l2, r2):
 
-   # Definition of the parameters
+  # If one rectangle is on left side of other
+  if(l1[0] > r2[0] or l2[0] > r1[0]):
+    return False
+
+  # If one rectangle is above other
+  if(l2[1] > r1[1] or l1[1] > r2[1]):
+    return False
+
+  return True
+
+
+def main(yolo):
   max_cosine_distance = 0.3
   nn_budget = None
   nms_max_overlap = 1.0
@@ -38,113 +51,168 @@ def main(yolo):
   tracker = Tracker(metric)
 
   writeVideo_flag = False
-  video_name = 'GroupWorkoutVideo.m4v'
   _fps = 10
   width = 320
   height = 240
+  count = 0
+  for video_location in glob.glob("../input/*.m4v"):
+    # Definition of the parameters
+    video_name = os.path.basename(video_location)
+    output_dir = os.path.join('../output', video_name)
 
-  output_dir = os.path.join('./output', video_name)
+    if os.path.exists(output_dir):
+      rmtree(output_dir)
 
-  if os.path.exists(output_dir):
-    rmtree(output_dir)
+    output_dir_raw = os.path.join(output_dir, 'raw')
+    output_dir_annotated = os.path.join(output_dir, 'annotated')
+    if not os.path.exists(output_dir_raw):
+      os.makedirs(output_dir_raw)
+    if not os.path.exists(output_dir_annotated):
+      os.makedirs(output_dir_annotated)
 
-  output_dir_raw = os.path.join(output_dir, 'raw')
-  output_dir_annotated = os.path.join(output_dir, 'annotated')
-  if not os.path.exists(output_dir_raw):
-    os.makedirs(output_dir_raw)
-  if not os.path.exists(output_dir_annotated):
-    os.makedirs(output_dir_annotated)
+    video_capture = cv2.VideoCapture(video_location)
+    video_capture.set(cv2.CAP_PROP_FPS, _fps)
+    video_capture.set(3, width)
+    video_capture.set(4, height)
 
-  video_capture = cv2.VideoCapture(video_name)
-  video_capture.set(cv2.CAP_PROP_FPS, _fps)
-  video_capture.set(3, width)
-  video_capture.set(4, height)
+    timestamps = [video_capture.get(cv2.CAP_PROP_POS_MSEC)]
+    calc_timestamps = [0.0]
+    #list_file = open(os.path.join(output_dir, 'metadata.txt'), 'w')
+    dfObj = pd.DataFrame(
+        columns=['FrameId', 'PersonId', 'x1', 'y1', 'x2', 'y2'])
 
-  timestamps = [video_capture.get(cv2.CAP_PROP_POS_MSEC)]
-  calc_timestamps = [0.0]
-  list_file = open(os.path.join(output_dir, 'metadata.txt'), 'w')
-  frame_index = -1
+    frame_index = -1
 
-  if writeVideo_flag:
-    # Define the codec and create VideoWriter object
-    w = int(video_capture.get(3))
-    h = int(video_capture.get(4))
-    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    out = cv2.VideoWriter(os.path.join(output_dir), fourcc, 15, (w, h))
+    if writeVideo_flag:
+      # Define the codec and create VideoWriter object
+      w = int(video_capture.get(3))
+      h = int(video_capture.get(4))
+      fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+      out = cv2.VideoWriter(os.path.join(output_dir), fourcc, 15, (w, h))
 
-  fps = 0.0
-  while True:
-    ret, frame = video_capture.read()  # frame shape 320*240*3
-    if ret != True:
-      break
-    t1 = time.time()
+    fps = 0.0
+    while True:
+      ret, frame = video_capture.read()  # frame shape 320*240*3
+      if ret != True:
+        break
+      t1 = time.time()
 
-    timestamps.append(video_capture.get(cv2.CAP_PROP_POS_MSEC))
-    calc_timestamps.append(int(calc_timestamps[-1] + 1000/_fps))
+      timestamps.append(video_capture.get(cv2.CAP_PROP_POS_MSEC))
+      calc_timestamps.append(int(calc_timestamps[-1] + 1000/_fps))
+      cv2.imwrite(os.path.join(output_dir_raw,
+                               str(calc_timestamps[-1]) + '.jpg'), frame)
+    # image = Image.fromarray(frame)
+      image = Image.fromarray(frame[..., ::-1])  # bgr to rgb
+      boxs = yolo.detect_image(image)
+    # print("box_num",len(boxs))
+      features = encoder(frame, boxs)
 
-   # image = Image.fromarray(frame)
-    image = Image.fromarray(frame[..., ::-1])  # bgr to rgb
-    boxs = yolo.detect_image(image)
-   # print("box_num",len(boxs))
-    features = encoder(frame, boxs)
+      # score to 1.0 here).
+      detections = [Detection(bbox, 1.0, feature)
+                    for bbox, feature in zip(boxs, features)]
 
-    # score to 1.0 here).
-    detections = [Detection(bbox, 1.0, feature)
-                  for bbox, feature in zip(boxs, features)]
+      # Run non-maxima suppression.
+      boxes = np.array([d.tlwh for d in detections])
+      scores = np.array([d.confidence for d in detections])
+      indices = preprocessing.non_max_suppression(
+          boxes, nms_max_overlap, scores)
+      detections = [detections[i] for i in indices]
 
-    # Run non-maxima suppression.
-    boxes = np.array([d.tlwh for d in detections])
-    scores = np.array([d.confidence for d in detections])
-    indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
-    detections = [detections[i] for i in indices]
+      # Call the tracker
+      tracker.predict()
+      tracker.update(detections)
 
-    # Call the tracker
-    tracker.predict()
-    tracker.update(detections)
-    '''
+      # to find the boxes
+      occupied_boxes = []
+      final_box = []
+      for track in tracker.tracks:
+        if not track.is_confirmed() or track.time_since_update > 1:
+          continue
+        area = [0]*len(detections)
+        _t = track.to_tlbr()
+        if len(detections) != 0:
+          for i in range(0, len(detections)):
+            _boxs = detections[i].to_tlbr()
+            if not doOverlap((_boxs[0], _boxs[1]), (_boxs[2], _boxs[3]),
+                             (_t[0], _t[1]), (_t[2], _t[3])):
+              continue
+            y_axis = sorted([_t[1], _t[3], _boxs[1], _boxs[3]])
+            x_axis = sorted([_t[0], _t[2], _boxs[0], _boxs[2]])
+            if i not in occupied_boxes:
+              area[i] = abs((y_axis[2] - y_axis[1])*(x_axis[2] - x_axis[1]))
+          person = - \
+              1 if area.index(max(area)) == 0 and max(
+                  area) == 0 else area.index(max(area))
+          if person > -1:
+            occupied_boxes.append(person)
+            _d = detections[person].to_tlbr()
+            final_box.append((track.track_id, _d))
+
+          # list_file.write(str(boxs[i][0]) + ' '+str(boxs[i][1]) + ' '+str(boxs[i][2]) + ' '+str(boxs[i][3]) + ' ')
+      for p in final_box:
+        cv2.putText(frame, '________' +
+                    str(p[0]), (int(p[1][0]), int(p[1][1])), 0, 5e-3 * 200, (0, 255, 0), 2)
+        dfObj = dfObj.append({
+            'FrameId': calc_timestamps[-1],
+            'PersonId': p[0],
+            'x1': int(p[1][0]),
+            'y1': int(p[1][1]),
+            'x2': int(p[1][2]),
+            'y2': int(p[1][3])
+        }, ignore_index=True)
+
+        #list_file.write(str(calc_timestamps[-1]) + '\t' + str(p[0]) + '\t' + str(int(p[1][0])), str(int(p[1][1])) + '\t' + str(int(p[1][2])) + '\t' + str(int(p[1][3])))
+
+      # list_file.write('\n')
+      dfObj.to_csv(os.path.join(output_dir, 'metadata.txt'))
+      # validation
+      if True:
         for track in tracker.tracks:
-            if not track.is_confirmed() or track.time_since_update > 1:
-                continue 
-            bbox = track.to_tlbr()
-            #cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
-            cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
+          if not track.is_confirmed() or track.time_since_update > 1:
+            continue
+          bbox = track.to_tlbr()
+          cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(
+              bbox[2]), int(bbox[3])), (255, 255, 255), 2)
+          cv2.putText(frame, str(track.track_id), (int(
+              bbox[0]), int(bbox[1])), 0, 5e-3 * 200, (0, 255, 0), 2)
 
         for det in detections:
-            bbox = det.to_tlbr()
-            cv2.rectangle(frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 2)
-        '''
-    # cv2.imshow('', frame)
-    # save frame as JPEG file
-    cv2.imwrite(os.path.join(output_dir_raw,
-                             str(calc_timestamps[-1]) + '.jpg'), frame)
+          bbox = det.to_tlbr()
+          cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(
+              bbox[2]), int(bbox[3])), (255, 0, 0), 2)
 
-    if True:
+        # cv2.imshow('', frame)
+        # save frame as JPEG file
+        cv2.imwrite(os.path.join(output_dir_annotated,
+                                 str(calc_timestamps[-1]) + '.jpg'), frame)
+
       # save a frame
       # out.write(frame)
-      frame_index = frame_index + 1
-      list_file.write(str(frame_index)+' ')
-      if len(boxs) != 0:
-        for i in range(0, len(boxs)):
-          list_file.write(str(boxs[i][0]) + ' '+str(boxs[i][1]) +
-                          ' '+str(boxs[i][2]) + ' '+str(boxs[i][3]) + ' ')
-      list_file.write('\n')
+      # frame_index = frame_index + 1
+      # list_file.write(str(frame_index)+' ')
+      # if len(boxs) != 0:
+      #   for i in range(0, len(boxs)):
+      #     list_file.write(str(boxs[i][0]) + ' '+str(boxs[i][1]) +
+      #                     ' '+str(boxs[i][2]) + ' '+str(boxs[i][3]) + ' ')
+      # list_file.write('\n')
 
-    list_file.flush()
-    os.fsync(list_file.fileno())
+      # list_file.flush()
+      # os.fsync(list_file.fileno())
 
-    fps = (fps + (1./(time.time()-t1))) / 2
-    print("fps= %f" % (fps))
+      if count % 200 == 0:
+        fps = (fps + (1./(time.time()-t1))) / 2
+        print("fps= %f" % (fps))
+      count += 1
+      # Press Q to stop!
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    # Press Q to stop!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
+    video_capture.release()
+    if writeVideo_flag:
+      out.release()
 
-  video_capture.release()
-  if writeVideo_flag:
-    out.release()
-
-  list_file.close()
-  cv2.destroyAllWindows()
+    # list_file.close()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
